24985808feb479ba1690f4ccc6b53e1f11cfb88f
David Ogor <doctown@yahoo.com>
2016-03-25 16:23:52 -0700

    Initial implmentation of responses for getting and posting data.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,25 @@
 var models = require('../models');
+var app = require('../app');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      // create an empty array
+      var messages = [];
+      // call models.messages.get();
+      messages = models.messages.get();
+      // Stringify the array
+      // Send the array back to the user
+      res.status(200).json(JSON.stringify(messages)).end();
+    }, // a function which handles a get request for all messages
+    post: function (req, res) { // a function which handles posting a message to the database
+      // Get the data from the post request
+      var body = req.body;
+      // Call models.messages.post(data);
+      models.messages.post(JSON.parse(body));
+      // Reply with object creation response code
+      res.status(201).end();
+    }
   },
 
   users: {
