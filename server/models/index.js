020b4250b4379b096e8e772a1d0a8b963caf2821
David Ogor <doctown@yahoo.com>
2016-03-27 10:54:19 -0700

    Nest the post message logic

--- server/models/index.js
+++ server/models/index.js
@@ -4,61 +4,6 @@ module.exports = {
   messages: {
     get: function (callback) { // a function which produces all the messages
       db.query('SELECT messages.id, messages.createDateTime, messages.message, rooms.name, username.username FROM username, message_username, messages, message_room, rooms WHERE messages.id = message_room.id_message AND message_room.id_room = rooms.id AND username.id = message_username.id_username AND message_username.id_message = messages.id', callback);
-      // Select all messages from messagesRoom
-
-    /*  db.query('SELECT * FROM messages', function(err, result) {
-        if (err) {
-          throw err;
-        }
-
-        result.forEach(function(message) {
-          var msg = {};
-          msg.id = message.id;
-          msg.createDateTime = message.createDateTime;
-          msg.message = message.message;
-          messages.push(msg);
-        });
-      });
-
-      // Select all rooms
-      db.query('SELECT * FROM rooms', function(err, result) {
-        if (err) {
-          throw err;
-        }
-        // create a rooms object for each room
-        result.forEach(function(room) {
-          rooms[room.id] = room.name;
-        });
-      });
-
-      // Select all users
-      db.query('SELECT * FROM username', function(err, result) {
-        if (err) {
-          throw err;
-        }
-        // create a user object for each user
-        result.forEach(function(username) {
-          usernames[username.id] = username.username;
-        });
-      });
-
-      db.query('SELECT * FROM message_username', function(err, result) {
-        if (err) {
-          throw err;
-        }
-        result.forEach(function(message) {
-          messagesUsername[message.id_message] = usernames[message.id_username];
-        });
-      });
-
-      db.query('SELECT * FROM message_room', function(err, result) {
-        if (err) {
-          throw err;
-        }
-        result.forEach(function(message) {
-          messagesRoom[message.id_message] = rooms[message.id_room];
-        });
-      });*/
     },
     post: function (message) { // a function which can be used to insert a message into the database
       var usernameID, roomID, messageID;
@@ -70,44 +15,43 @@ module.exports = {
         }
         // Get the messages id number
         usernameID = result[0].id;
-      });
 
-      // Get the rooms id number
-      db.query('SELECT id FROM rooms WHERE name=?', [message.roomname], function(err, result) {
-        if (err) {
-          throw err;
-        }
-        // Get the messages id number
-        roomID = result[0].id;
-      });
+        // Get the rooms id number
+        db.query('SELECT id FROM rooms WHERE name=?', [message.roomname], function(err, result) {
+          if (err) {
+            throw err;
+          }
+          // Get the messages id number
+          roomID = result[0].id;
 
-      // Insert the message in the database
-      db.query('INSERT INTO messages SET message=?', [message.message], function(err, result) {
-        if (err) {
-          throw err;
-        }
-        // Get the messages id number
-        messageID = result.insertId;
-      });
+          // Insert the message in the database
+          db.query('INSERT INTO messages SET message=?', [message.message], function(err, result) {
+            if (err) {
+              throw err;
+            }
+            // Get the messages id number
+            messageID = result.insertId;
+          });
 
-      // Insert the message id / room id to messagesRoom
-      db.query('INSERT INTO message_room SET id_message=?, id_room=?', [messageID, roomID], function(err, result) {
-        if (err) {
-          throw err;
-        }
-      });
+          // Insert the message id / room id to messagesRoom
+          db.query('INSERT INTO message_room SET id_message=?, id_room=?', [messageID, roomID], function(err, result) {
+            if (err) {
+              throw err;
+            }
+          });
 
-      // Insert the message id / username id to username_message
-      db.query('INSERT INTO message_username SET id_message=?, id_username=?', [messageID, usernameID], function(err, result) {
-        if (err) {
-          throw err;
-        }
+          // Insert the message id / username id to username_message
+          db.query('INSERT INTO message_username SET id_message=?, id_username=?', [messageID, usernameID], function(err, result) {
+            if (err) {
+              throw err;
+            }
+          });
+        });
       });
     }
   },
 
   users: {
-    // Ditto as above.
     get: function () {
 
     },
